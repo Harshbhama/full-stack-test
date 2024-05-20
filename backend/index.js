cd60de7af22a6571fbab358fe0c39a266fca82c8
Harsh Bhama <harshbhama96@gmail.com>
2024-05-20 16:28:32 +0530

    update cors ip

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ application.use(bodyparser.urlencoded({
 require('dotenv').config();
 var corsOptions = {
     credentials: true,
-    origin: 'http://13.232.73.226:3000' };
+    origin: 'http://15.206.82.156:3000' };
 application.use(cors(corsOptions));
 application.use(cookies())
 application.use(bodyparser.json({limit: '2mb'}))
